8c27803fff550416cd7eafca24d63ed82abf0448
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-06-13 17:00:45 -0400

    Tweak headline

--- src/components/Main.js
+++ src/components/Main.js
@@ -27,7 +27,7 @@ class Main extends React.Component {
       <div className="container__outer">
         <div className="container__inner">
           <Header
-            headline="Contact tracers needed in Pennsylvania"
+            headline="How many contact tracers does Pennsylvania need?"
             subtitle="As of June 8, public health experts estimate that Pennsylvania needs nearly 4,000 contact tracers to reach 
             the contacts of all new COVID-19 cases within one week. Estimates are based on the number of new cases in 
             each county over the past 14 days."
