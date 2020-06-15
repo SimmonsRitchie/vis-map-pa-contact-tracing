7766d56623b6e18bf2525660db56dfbc088f421f
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-06-15 15:23:00 -0400

    Integrate case data for june 8

--- src/utils/load.js
+++ src/utils/load.js
@@ -1,17 +1,27 @@
 import { csv } from "d3-fetch";
-import DUMMY_DATA from "../data/pa_contact_tracers.csv";
+import TRACERS_NEEDED from "../data/pa_contact_tracers.csv";
+import CASES_14_DAYS from "../data/pa_cases_14_days_prior_to_06-08-2020.csv";
+import joinData from "./join";
 
 const loadData = () => {
   /* Fetch and parse files. */
-  return Promise.all([csv(DUMMY_DATA), import("~/data/pa-county.json")]).then(
-    ([contactTracerData, paCounties]) => {
-      const data = {};
-      data.contactTracerData = contactTracerData;
-      data.geoData = paCounties;
-      // data.geoData = topoToGeo(paCounties, "pa-county");
-      return data;
-    }
-  );
+  return Promise.all([
+    csv(TRACERS_NEEDED),
+    csv(CASES_14_DAYS),
+    import("~/data/pa-county.json"),
+  ]).then(([tracersNeeded, newCases, paCounties]) => {
+    const data = {};
+    data.contactTracerData = joinData({
+      dataset1: tracersNeeded,
+      dataset2: newCases,
+      leftJoinOn: "county",
+      rightJoinOn: "county",
+      appendKey: "new_cases",
+    });
+    data.geoData = paCounties;
+    // data.geoData = topoToGeo(paCounties, "pa-county");
+    return data;
+  });
 };
 
 export default loadData;
